a454dbc504ad013f988c27b9fa8a2476490522b7
gitalvininfo <yansonalvin29@gmail.com>
2018-03-07 14:56:04 +0800

    routed after sign

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyBB-hesh2cfcZUcvt3t1LFjxFrRT3QniRU",
+        authDomain: "angularsocial-a2e09.firebaseapp.com",
+        databaseURL: "https://angularsocial-a2e09.firebaseio.com",
+        projectId: "angularsocial-a2e09",
+        storageBucket: "angularsocial-a2e09.appspot.com",
+        messagingSenderId: "489605180268"
     }
 };
