599399654b017e5bf4b44b7fcb3cc23fd816eeac
gitkencay <ken05cay@gmail.com>
2018-03-08 08:23:32 +0700

    Done working with cover upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyBB-hesh2cfcZUcvt3t1LFjxFrRT3QniRU",
-        authDomain: "angularsocial-a2e09.firebaseapp.com",
-        databaseURL: "https://angularsocial-a2e09.firebaseio.com",
-        projectId: "angularsocial-a2e09",
-        storageBucket: "angularsocial-a2e09.appspot.com",
-        messagingSenderId: "489605180268"
+        apiKey: "AIzaSyC5lJra_1yVraJDeDuFUTOjrdGd1nYNVyI",
+        authDomain: "angular-social-d897c.firebaseapp.com",
+        databaseURL: "https://angular-social-d897c.firebaseio.com",
+        projectId: "angular-social-d897c",
+        storageBucket: "angular-social-d897c.appspot.com",
+        messagingSenderId: "806726911085"
     }
 };
